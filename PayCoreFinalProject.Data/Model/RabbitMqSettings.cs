86a74e8c8ef4d840bd03f48ed9ef8b38bc535852
Alp Artun Güvendiren <aartung@Alp-MacBook-Pro.local>
2022-09-21 20:01:59 +0300

    consumer added successfully

---
+++ PayCoreFinalProject.Data/Model/RabbitMqSettings.cs
@@ -0,0 +1,8 @@
+namespace PayCoreFinalProject.Data.Model;
+
+public class RabitMqSettings
+{
+    public string HostName { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
